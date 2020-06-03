b2310a6844d84ee22c0f3b521e572fe31935d278
mokrijyurij <mokrijyurij@outlook.com>
2020-06-03 11:50:12 +0300

    set pages

--- app.js
+++ app.js
@@ -9,6 +9,23 @@ const urlencodedParser = bodyParser.urlencoded({extended: false});
 app.get("/register", urlencodedParser, function (request, response) {
     response.sendFile(__dirname + "/register.html");
 });
+
+app.get("/css/owl.carousel.css", urlencodedParser, function (request, response) {
+    response.sendFile(__dirname + "/css/owl.carousel.css");
+});
+
+app.get("/css/bootstrap-3.1.1.min.css", urlencodedParser, function (request, response) {
+    response.sendFile(__dirname + "/css/bootstrap-3.1.1.min.css");
+});
+
+app.get("/css/style.css", urlencodedParser, function (request, response) {
+    response.sendFile(__dirname + "/css/style.css");
+});
+
+app.get("/about", urlencodedParser, function (request, response) {
+    response.sendFile(__dirname + "/about.html");
+});
+
 app.post("/register", urlencodedParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     console.log(request.body);
