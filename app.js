bc4408b27de035c642af4c3d400b97a34e3053af
Yurij <nabor_2008@i.ua>
2020-06-04 16:04:57 +0300

    add admin

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ app.use(express.static(__dirname + "/"))
 app.use(express.static(__dirname + "/css"))
 app.use(express.static(__dirname + "/images"))
 app.use(express.static(__dirname + "/js"))
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/admin"))
 
 app.listen(3000);
