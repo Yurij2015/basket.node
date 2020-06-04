312adc07403dc0d0916d23745fc93fff574ebfa7
Yurij <nabor_2008@i.ua>
2020-06-04 15:47:58 +0300

    add static page to app.js

--- app.js
+++ app.js
@@ -1,39 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+// // создаем парсер для данных application/x-www-form-urlencoded
+// const urlencodedParser = bodyParser.urlencoded({extended: false});
+//
+// app.get("/register", urlencodedParser, function (request, response) {
+//     response.sendFile(__dirname + "/register.html");
+// });
+//
+// app.get("/css/owl.carousel.css", urlencodedParser, function (request, response) {
+//     response.sendFile(__dirname + "/css/owl.carousel.css");
+// });
+//
+// app.get("/css/bootstrap-3.1.1.min.css", urlencodedParser, function (request, response) {
+//     response.sendFile(__dirname + "/css/bootstrap-3.1.1.min.css");
+// });
+//
+// app.get("/css/style.css", urlencodedParser, function (request, response) {
+//     response.sendFile(__dirname + "/css/style.css");
+// });
+//
+// app.get("/about", urlencodedParser, function (request, response) {
+//     response.sendFile(__dirname + "/about.html");
+// });
+//
+// app.post("/register", urlencodedParser, function (request, response) {
+//     if (!request.body) return response.sendStatus(400);
+//     console.log(request.body);
+//     response.send(`${request.body.userName} - ${request.body.userAge}`);
+// });
+//
+// // app.use(function (request, response) {
+// //     response.sendFile(__dirname+"/index.html");
+// // });
 
-// создаем парсер для данных application/x-www-form-urlencoded
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+// app.use("/", function(request, response){
+//     response.send("<h1>Главная страница</h1>");
+// });
 
-app.get("/register", urlencodedParser, function (request, response) {
-    response.sendFile(__dirname + "/register.html");
-});
-
-app.get("/css/owl.carousel.css", urlencodedParser, function (request, response) {
-    response.sendFile(__dirname + "/css/owl.carousel.css");
-});
-
-app.get("/css/bootstrap-3.1.1.min.css", urlencodedParser, function (request, response) {
-    response.sendFile(__dirname + "/css/bootstrap-3.1.1.min.css");
-});
-
-app.get("/css/style.css", urlencodedParser, function (request, response) {
-    response.sendFile(__dirname + "/css/style.css");
-});
-
-app.get("/about", urlencodedParser, function (request, response) {
-    response.sendFile(__dirname + "/about.html");
-});
-
-app.post("/register", urlencodedParser, function (request, response) {
-    if (!request.body) return response.sendStatus(400);
-    console.log(request.body);
-    response.send(`${request.body.userName} - ${request.body.userAge}`);
-});
-
-app.get("/", function (request, response) {
-    response.send("Главная страница");
-});
+app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/css"))
+app.use(express.static(__dirname + "/images"))
+app.use(express.static(__dirname + "/js"))
+app.use(express.static(__dirname + "/"))
 
 app.listen(3000);
